8b95e18fe5cb8791827562aab0e3b2a1bfb1a10d
Corey DeRossett <cderossett3@gmail.com>
2024-02-07 10:44:42 -0500

    added pseudo code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+       //main()
+        //		num birthMonth
+        //		output “Enter your birth month using a number 1-12:”
+        //		input birthMonth
+        //		if birthMonth > 0 AND birthMonth < 13 then
+        //			output “Your birth month is: N”
+        //		else
+        //			output “You entered an incorrect month value: N”
+        //		end if
+        //	return
+        //end class
     }
 }
