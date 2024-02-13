dc1613b0f15b1cfb13ef6ae10fb4e155a1907bb3
Corey DeRossett <cderossett3@gmail.com>
2024-02-12 20:38:13 -0500

    fixed scanner mistakes

--- src/Main.java
+++ src/Main.java
@@ -18,18 +18,23 @@ public class Main {
         //Declaring variables
         Scanner in = new Scanner(System.in);
         int birthMonth = 0;
+        String trash = "";
         //output asking for birth month integer and input from user
         System.out.println("Enter your birth month using a number 1-12:");
-        birthMonth = in.nextInt();
-        //clear buffer
-        in.nextLine();
-        //if statements that sets parameters of valid birth month integers and outputs according to integer entered
-        if (birthMonth > 0 && birthMonth < 13) {
-            System.out.println("Your birth month is: " + birthMonth);
+        //if statement that allows valid integer to be scanned in and an else statement that indicates the input is invalid
+        if (in.hasNextInt()) {
+            birthMonth = in.nextInt();
+            //clear buffer
+            in.nextLine();
+            //if statements that sets parameters of valid birth month integers and outputs according to integer entered
+            if (birthMonth > 0 && birthMonth < 13) {
+                System.out.println("Your birth month is: " + birthMonth);
+            }
         }
         else
         {
-            System.out.println("You entered an incorrect month value: " + birthMonth);
+            trash = in.nextLine();
+            System.out.println("You must enter a valid integer 1-12, not: " +trash);
         }
     }
 }
